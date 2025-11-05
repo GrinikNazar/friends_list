7866641543b471855212772f10730ceeba38989a
GrinikNazar <hrynyknazar@gmail.com>
2025-11-06 00:11:59 +0200

    docker changes v6

--- app/db.py
+++ app/db.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 dynamo = boto3.resource(
     "dynamodb",
+    # endpoint_url=os.getenv("DYNAMO_ENDPOINT", None),
     region_name="eu-central-1",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
